59218c924e4fa6eda17ac4b52f286282276c479c
Dev-LucasLima <lucas.lima.1478@outlook.com>
2025-11-26 03:01:36 -0300

    feat: create UserRepository using JPA to manipulate database

---
+++ users/src/main/java/br/com/microservice/users/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package br.com.microservice.users.repository;
+
+import br.com.microservice.users.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+
+}
