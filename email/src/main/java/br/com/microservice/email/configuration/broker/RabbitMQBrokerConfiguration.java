ab460fe30213d4fd4d3366714eb511085391588b
Dev-LucasLima <lucas.lima.1478@outlook.com>
2025-11-26 23:39:59 -0300

    feat: add Jackson2JsonMessageConverter to converter receiver message in DTO

--- email/src/main/java/br/com/microservice/email/configuration/broker/RabbitMQBrokerConfiguration.java
+++ email/src/main/java/br/com/microservice/email/configuration/broker/RabbitMQBrokerConfiguration.java
@@ -1,20 +1,25 @@
 package br.com.microservice.email.configuration.broker;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import jakarta.validation.Valid;
-
 @Configuration
-public class RabbitMQBrokkerConfiguration {
+public class RabbitMQBrokerConfiguration {
 
     @Value("${broker.queue.email.name}")
     private String queue;
-    
+
     @Bean
     public Queue emailQueue() {
         return new Queue(queue, true);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter(new ObjectMapper());
     }
 }
