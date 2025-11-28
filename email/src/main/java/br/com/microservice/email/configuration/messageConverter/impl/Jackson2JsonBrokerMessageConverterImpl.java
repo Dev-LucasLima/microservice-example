a60047e60c55ea0f275da3e15845764fec28cab9
Dev-LucasLima <lucas.lima.1478@outlook.com>
2025-11-27 23:52:14 -0300

    refactor: rename class

--- email/src/main/java/br/com/microservice/email/configuration/messageConverter/impl/Jackson2JsonBrokerMessageConverterImpl.java
+++ email/src/main/java/br/com/microservice/email/configuration/messageConverter/impl/Jackson2JsonBrokerMessageConverterImpl.java
@@ -7,7 +7,7 @@ import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.stereotype.Component;
 
 @Component
-public class JacksonBrokerMessageConverterImpl implements BrokerMessageConverter {
+public class Jackson2JsonBrokerMessageConverterImpl implements BrokerMessageConverter {
 
     @Override
     public MessageConverter createMessageConverter() {
