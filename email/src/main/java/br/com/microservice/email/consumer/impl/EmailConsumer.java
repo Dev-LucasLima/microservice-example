9697d9b5009453d6c714365dc178b8282d0fd598
Dev-LucasLima <lucas.lima.1478@outlook.com>
2025-11-29 11:47:48 -0300

    feat: define EmailConsumer to implement BrokerConsumer

--- email/src/main/java/br/com/microservice/email/consumer/impl/EmailConsumer.java
+++ email/src/main/java/br/com/microservice/email/consumer/impl/EmailConsumer.java
@@ -1,6 +1,7 @@
-package br.com.microservice.email.consumer;
+package br.com.microservice.email.consumer.impl;
 
 import br.com.microservice.email.broker.queue.QueueProvider;
+import br.com.microservice.email.consumer.BrokerConsumer;
 import br.com.microservice.email.dto.EmailRecordDto;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,13 +9,14 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
-public class EmailConsumer {
+public class EmailConsumer implements BrokerConsumer<EmailRecordDto> {
 
     @Autowired
     private QueueProvider queueProvider;
 
+    @Override
     @RabbitListener(queues = "#{@queueProvider.getQueueName()}")
-    public void receiveEmail(@Payload final EmailRecordDto emailRecordDto) {
+    public void receiveMessage(@Payload final EmailRecordDto emailRecordDto) {
         System.out.println("Received email message: " + emailRecordDto.text());
     }
 
