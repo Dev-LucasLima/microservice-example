92dae19476ddaefa08cd49a5235bfe1f4c39aa03
Dev-LucasLima <lucas.lima.1478@outlook.com>
2025-11-29 15:26:55 -0300

    feat: create email from provider class

---
+++ email/src/main/java/br/com/microservice/email/email/provider/EmailFromProvider.java
@@ -0,0 +1,7 @@
+package br.com.microservice.email.email.provider;
+
+public interface EmailFromProvider {
+
+    String getEmailFromAddress();
+
+}
---
+++ email/src/main/java/br/com/microservice/email/email/provider/impl/EmailFromProviderImpl.java
@@ -0,0 +1,17 @@
+package br.com.microservice.email.email.provider.impl;
+
+import br.com.microservice.email.email.provider.EmailFromProvider;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component("mailFromProvider")
+public class EmailFromProviderImpl implements EmailFromProvider {
+
+    @Value("${spring.mail.username}")
+    private String emailFromAddress;
+
+    @Override
+    public String getEmailFromAddress() {
+        return emailFromAddress;
+    }
+}
