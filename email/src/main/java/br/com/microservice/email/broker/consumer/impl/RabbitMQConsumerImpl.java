dfad84e2f3331722d332e7e507f4c3875ecfc192
Dev-LucasLima <lucas.lima.1478@outlook.com>
2025-11-30 12:11:57 -0300

    feat: use ManagerEmailService to send and save email received by RabbitMQListener

--- email/src/main/java/br/com/microservice/email/broker/consumer/impl/RabbitMQConsumerImpl.java
+++ email/src/main/java/br/com/microservice/email/broker/consumer/impl/RabbitMQConsumerImpl.java
@@ -3,7 +3,10 @@ package br.com.microservice.email.broker.consumer.impl;
 import br.com.microservice.email.broker.consumer.BrokerConsumer;
 import br.com.microservice.email.broker.queue.QueueProvider;
 import br.com.microservice.email.dto.EmailRecordDto;
+import br.com.microservice.email.model.EmailModel;
+import br.com.microservice.email.service.ManagerEmailService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
@@ -12,12 +15,18 @@ import org.springframework.stereotype.Component;
 public class RabbitMQConsumerImpl implements BrokerConsumer<EmailRecordDto> {
 
     @Autowired
+    private ManagerEmailService managerEmailService;
+
+    @Autowired
     private QueueProvider queueProvider;
 
     @Override
     @RabbitListener(queues = "#{@queueProvider.getQueueName()}")
     public void receiveMessage(@Payload final EmailRecordDto emailRecordDto) {
-        System.out.println("Received email message: " + emailRecordDto.text());
+        final EmailModel emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailRecordDto, emailModel);
+
+        managerEmailService.sendAndSaveEmail(emailModel);
     }
 
 }
