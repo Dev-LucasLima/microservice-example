e740155b896c6c238f0683956d9fb77c25f45ff6
Dev-LucasLima <lucas.lima.1478@outlook.com>
2025-11-30 11:08:50 -0300

    feat: add @Component annotation in CustomMailMessageImpl

--- email/src/main/java/br/com/microservice/email/email/message/impl/CustomMailMessageImpl.java
+++ email/src/main/java/br/com/microservice/email/email/message/impl/CustomMailMessageImpl.java
@@ -3,8 +3,11 @@ package br.com.microservice.email.email.message.impl;
 import br.com.microservice.email.email.message.CustomMailMessage;
 import br.com.microservice.email.model.EmailModel;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CustomMailMessageImpl implements CustomMailMessage {
+
     @Override
     public SimpleMailMessage createMailMessage(final EmailModel emailModel) {
         return new SimpleMailMessage() {{
